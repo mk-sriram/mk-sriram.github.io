69e3586e0f02161f108abf18f1cc788f7d46a454
Sriram MK <mksriram@umich.edu>
2024-01-20 15:45:50 -0500

    Revert "Added projects and contact page. ( contact page does not work )"
    This reverts commit e1646f856ac2d5fbcb8613b739d9b09a28e41c1d.

--- con444tact.js
+++
@@ -1,6 +0,0 @@
-const subject = document.getElementsByClassName("subject-input");
-const email = document.getElementsByClassName("email-input");
-const Message = document.getElementsByClassName("text-area");
-
-
-
